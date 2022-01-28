669da5aed92167f55840b4bf778778d1d965a53e
bpsrGIT <ramon.sp.baltazar@gmail.com>
2022-01-28 18:19:03 +0800

    initial commit

---
+++ Carousel/carousel.js
@@ -0,0 +1,17 @@
+const image = document.querySelector(".image");
+const carouselBack = document.querySelector(".back");
+const carouselForward= document.querySelector(".forward")
+
+
+function toggleForward(){
+
+}
+
+function toggleBack(){
+
+}
+
+carouselBack.addEventListener('click', toggleBack);
+carouselForward.addEventListener('click', toggleForward);
+
+
---
+++ Header/header.js
@@ -0,0 +1,23 @@
+const header = document.querySelector(".nav-container");
+const hamburger = document.querySelector(".nav-mobile-btn");
+const mobileNav = document.querySelector("#mobile-nav");
+const toOpen = document.querySelector(".toOpen");
+const toClose = document.querySelector(".toClose");
+
+function toggleMenu() {
+  if (hamburger.classList.contains("showMenu")) {
+    hamburger.classList.remove("showMenu");
+    header.style.height = "64px";
+    mobileNav.style.display = "none";
+    toOpen.style.display = "block";
+    toClose.style.display = "none";
+  } else {
+    hamburger.classList.add("showMenu");
+    header.style.height = "441px";
+    mobileNav.style.display = "block";
+    toOpen.style.display = "none";
+    toClose.style.display = "block";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
